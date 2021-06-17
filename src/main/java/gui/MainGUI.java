eb58f1fdc0d58c59280f5059e90ca70d854d2141
mikosz08 <ziolek.mikosz@protonmail.com>
2021-06-18 01:52:59 +0200

    first qui proto

---
+++ src/main/java/gui/MainGUI.java
@@ -0,0 +1,37 @@
+package gui;
+
+import javax.swing.*;
+
+public class MainGUI {
+
+    private JPanel mainPanel;
+    private JPanel tablesPanel;
+
+    private JPanel northTablePanel;
+    private JButton guildsButton;
+    private JButton membersButton;
+    private JButton eventsButton;
+
+    private JPanel centerTablePanel;
+    private JTable mainTable;
+
+    private JPanel menuPanel;
+
+    private JPanel southMenuPanel;
+    private JButton createGuildButton;
+    private JButton applyToGuildButton;
+    private JButton removeApplicationButton;
+    private JButton editDayMessageButton;
+    private JButton viewRankingButton;
+    private JButton createEventButton;
+    private JButton addApplicantButton;
+    private JButton kickMemberButton;
+    private JButton editDaySentenceButton;
+    private JButton deleteGuildButton;
+
+    private JPanel northMenuPanel;
+    private JTextArea guildInfoTextArea;
+
+    private JPanel southTablePanel;
+    private JTextArea logTextArea;
+}
